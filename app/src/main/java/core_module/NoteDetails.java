03b0ff13864147d692ca5e0bd9ed1b4443248b27
Aryan Kumar <aryan68125@gmail.com>
2020-12-20 18:30:43 +0530

    version 2 export feature added in text format

--- app/src/main/java/core_module/NoteDetails.java
+++ app/src/main/java/core_module/NoteDetails.java
@@ -1,6 +1,11 @@
 package core_module;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.aditya.takingnotes.R;
@@ -10,6 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Environment;
 import android.speech.tts.TextToSpeech;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -19,6 +25,10 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class NoteDetails extends AppCompatActivity {
@@ -31,6 +41,10 @@ public class NoteDetails extends AppCompatActivity {
     //setting up text to speech listener
     TextToSpeech mtts;
     int everythingIsOKmttsIsGoodToGo = 0;
+
+    //creating a variable to store data while writing
+    private static final int WRITE_EXTERNAL_STORAGE_CODE = 1;
+    String save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,6 +146,9 @@ public class NoteDetails extends AppCompatActivity {
                 mtts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
             }
         }
+        if(item.getItemId()==R.id.export){
+            scorestore();
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -170,4 +187,89 @@ public class NoteDetails extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
+
+
+    //storing high score int he file system you need to go to android manifest xml file
+    //there inside the application tag write (android:requestLegacyExternalStorage="true")
+    //permission denied problem will be solved
+    public void scorestore() //it will store the high score
+    {
+        save = contentTextView.getText().toString();
+        //data entry will take place
+            //android version above marshmallow will require to ask for user permission to access files
+            //go to manifest file and type the following under the package name
+            //<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"></uses-permission>
+
+            if(Build.VERSION.SDK_INT>Build.VERSION_CODES.M)  //checking our bhuild version of the os greater than marshmallow version
+            {
+                if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED)
+                {
+                    String [] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                    //show popup for runtime permissions
+                    requestPermissions(permissions, WRITE_EXTERNAL_STORAGE_CODE);
+                }
+                else{
+                    //permission already granted; hence save data
+                    saveToTxtFile(save);
+                }
+            }
+
+        }
+
+
+    //checking permission and requesting permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode)
+        {
+            case WRITE_EXTERNAL_STORAGE_CODE:{
+                // if request is canceled then result arrays are empty
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    saveToTxtFile(save);
+                }
+                else
+                {
+                    Toast.makeText(this, "Storage permission required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    //actually saving in game data onto the file system by creating a directory and a txt file
+    private void saveToTxtFile(String save) {
+        //get current Time for file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis());
+
+        try{
+            //path to storage in the file system
+            //File path = Environment.getExternalStorageDirectory();
+            File path = Environment.getExternalStorageDirectory();
+            //create folder name "Brain_Trainer_game"
+            File dir = new File(path+"/Taking_Notes/");
+            dir.mkdirs();
+
+            //File name
+            String Filename = titleTextView.getText().toString() + timeStamp + ".txt";
+
+            //creating new file
+            File file = new File(dir, Filename);
+
+            //FileWriter class s used to store characters in file
+            FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            bw.write(save);
+            bw.close();
+
+            Toast.makeText(this,Filename + "is saved\n" +dir, Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            //if anything goes wrong
+            Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }

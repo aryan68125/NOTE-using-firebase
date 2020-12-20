85ffd77ee58813324c25544fdaa88fbac7f8f0b6
Aryan Kumar <aryan68125@gmail.com>
2020-12-20 16:38:42 +0530

    note using firebase online database

--- app/src/main/java/core_module/NoteDetails.java
+++ app/src/main/java/core_module/NoteDetails.java
@@ -154,8 +154,8 @@ public class NoteDetails extends AppCompatActivity {
                         // Set the content to appear under the system bars so that the
                         // content doesn't resize when the system bars hide and show.
                         //  | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        //  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        //   | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                           | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         // Hide the nav bar and status bar
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);

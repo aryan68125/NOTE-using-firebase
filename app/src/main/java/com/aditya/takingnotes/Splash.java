fbd08c97fa28e076d3ef57c879ae8c1fc62f97e5
Aryan Kumar <aryan68125@gmail.com>
2021-03-27 23:54:22 +0530

    version 2 export feature added in text format

--- app/src/main/java/com/aditya/takingnotes/Splash.java
+++ app/src/main/java/com/aditya/takingnotes/Splash.java
@@ -126,47 +126,11 @@ service cloud.firestore {
 
     }
 
-    //the code below will hide the system navigation bar and system status bar for IMMERSIVE EXPERIENCE
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideSystemUI();
-        }
-    }
-
     //check if the device is connected or not
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        //  | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        //  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        //   | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
-    // Shows the system bars by removing all the flags
-// except for the ones that make the content appear under the system bars.
-    private void showSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
--- app/src/main/java/core_module/DevActivity.java
+++ app/src/main/java/core_module/DevActivity.java
@@ -79,6 +79,14 @@ public class DevActivity extends AppCompatActivity {
         });
     }
 
+    //exit animation of currently active activity
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.slide_up,R.anim.slide_down);
+    }
+
     //it will handle the back button in the tool bar
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -89,41 +97,6 @@ public class DevActivity extends AppCompatActivity {
             overridePendingTransition(R.anim.slide_up,R.anim.slide_down);
         }
         return super.onOptionsItemSelected(item);
-    }
-    //the code below will hide the system navigation bar and system status bar for IMMERSIVE EXPERIENCE
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            hideSystemUI();
-        }
-    }
-
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
-                        //  | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        //  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        //   | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
-    // Shows the system bars by removing all the flags
-// except for the ones that make the content appear under the system bars.
-    private void showSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
 }
